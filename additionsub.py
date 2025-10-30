fe3695b0da17bd155b106a1227cd428872a25c77
Akash Athni <01fe24bca251@kletech.ac.in>
2025-10-31 00:31:42 +0530

    saving

--- additionsub.py
+++ additionsub.py
@@ -3,6 +3,10 @@ b = int(input("Enter second number: "))
 
 add = a + b
 sub = a - b
+mult = a * b
+div = a / b
 
 print("Addition:", add)
-print("Subtraction:", sub)
+print("Subtraction:",sub)
+print("multiplication:",mult)
+print("division:",div)
